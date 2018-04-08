0b1680ebdf106e8ecc76e256ade35b5b8618cfc2
abdulrafaymd <abdulrafaymd@gmail.com>
2018-04-08 11:02:00 +0530

    mvc app

---
+++ MVC5App/MVC5App/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5App.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        #region Sign In method.    
+        /// <summary>    
+        /// Sign In User method.    
+        /// </summary>    
+        /// <param name="username">Username parameter.</param>    
+        /// <param name="role_id">Role ID parameter</param>    
+        /// <param name="isPersistent">Is persistent parameter.</param>    
+        private void SignInUser(string username, int role_id, bool isPersistent)
+        {
+            // Initialization.    
+            var claims = new List<Claim>();
+            try
+            {
+                // Setting    
+                claims.Add(new Claim(ClaimTypes.Name, username));
+                claims.Add(new Claim(ClaimTypes.Role, role_id.ToString()));
+                var claimIdenties = new ClaimsIdentity(claims, DefaultAuthenticationTypes.ApplicationCookie);
+                var ctx = Request.GetOwinContext();
+                var authenticationManager = ctx.Authentication;
+                // Sign In.    
+                authenticationManager.SignIn(new AuthenticationProperties() { IsPersistent = isPersistent }, claimIdenties);
+            }
+            catch (Exception ex)
+            {
+                // Info    
+                throw ex;
+            }
+        }
+        #endregion
+    }
+}
